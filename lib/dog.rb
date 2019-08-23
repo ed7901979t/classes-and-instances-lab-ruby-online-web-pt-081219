0bd1d79c9f7605e5489404aba3f42194d7528758
clean-command-1851 <ed7901979t@gmail.com>
2019-08-23 16:18:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,13 @@
 class Dog    
   
-  fido= Dog.new   
-snoopy= Dog.new    
-lassie= Dog.new    
+   
   
 end
 
-    
+    fido= Dog.new   
+snoopy= Dog.new    
+lassie= Dog.new    
+  
 
 class Person
   
